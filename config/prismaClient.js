0d6537eb711f98f44b13a710b6fd3216fdee6d20
Lluís Galindo <llgalindoe@gmail.com>
2026-06-19 13:29:50 +0200

    classe divencres II

--- config/prismaClient.js
+++
@@ -1,6 +0,0 @@
-import 'dotenv/config'
-import { PrismaClient } from "@prisma/client" // Es la herramienta principal de Prisma para hacer peticiones
-import { PrismaPg } from "@prisma/adapter-pg" // adaptador de prisma para postgreSQL
-const adapter = new PrismaPg({ connectionString: process.env.DIRECT_URL })
-const prisma = new PrismaClient({ adapter })
-export default prisma
